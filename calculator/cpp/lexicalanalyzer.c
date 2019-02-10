991080cf41928ef8fb75dae58b222907ac57a3d7
sam <sam@sam.com>
2019-02-10 23:50:20 +0800

    add CMakeList

---
+++ calculator/cpp/lexicalanalyzer.c
@@ -0,0 +1,20 @@
+//
+// Created by sam on 2019-02-10.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <token.h>
+
+static char *st_line;
+static int st_line_pos;
+
+
+int main(int argc, char **argv) {
+    char buf[1024];
+    while (fgets(buf, 1024, stdin) != NULL) {
+
+    }
+    return 0;
+}
