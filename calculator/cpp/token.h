27dd35ed57461c3155f435c6e1e6955ec484d5db
samyunwei <259375702@qq.com>
2019-02-10 23:50:20 +0800

    add CMakeList

---
+++ calculator/cpp/token.h
@@ -0,0 +1,31 @@
+//
+// Created by sam on 2019-02-10.
+//
+
+#ifndef DESIGNCOMPLIER_TOKEN_H
+#define DESIGNCOMPLIER_TOKEN_H
+
+typedef enum {
+    BAD_TOKEN,
+    NUMBER_TOKEN,
+    ADD_OPERATOR_TOKEN,
+    SUB_OPERATOR_TOKEN,
+    MUL_OPERATOR_TOKEN,
+    DIV_OPERATOR_TOKEN,
+    END_OF_OPERATOR_TOKEN
+} TokenKind;
+
+#define MAX_TOKEN_SIZE (100)
+
+typedef struct {
+    TokenKind kind;
+    double value;
+    char str[MAX_TOKEN_SIZE];
+} Token;
+
+void set_line(char *line);
+
+void get_token(Token *token);
+
+
+#endif //DESIGNCOMPLIER_TOKEN_H
