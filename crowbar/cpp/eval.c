a345560371f8f75db9411de06921ea6de778e769
samyunwei <259375702@qq.com>
2019-03-28 23:52:50 +0800

    temp fin eval

--- crowbar/cpp/eval.c
+++ crowbar/cpp/eval.c
@@ -463,3 +463,174 @@ CRB_Value crb_eval_minus_expression(CRB_Interpreter *inter, LocalEnvironment *en
     }
     return result;
 }
+
+static LocalEnvironment *alloc_local_envirnoment() {
+    LocalEnvironment *ret;
+    ret = MEM_malloc(sizeof(LocalEnvironment));
+    ret->variable = NULL;
+    ret->global_variable = NULL;
+
+    return ret;
+}
+
+
+static void dispose_local_environment(CRB_Interpreter *inter, LocalEnvironment *env) {
+    while (env->variable) {
+        Variable *temp;
+        temp = env->variable;
+        if (env->variable->value.type == CRB_STRING_VALUE) {
+            crb_release_string(env->variable->value.u.string_value);
+        }
+        env->variable = temp->next;
+        MEM_free(temp);
+    }
+    while (env->global_variable) {
+        GlobalVariableRef *ref;
+        ref = env->global_variable;
+        env->global_variable = ref->next;
+        MEM_free(ref);
+    }
+    MEM_free(env);
+}
+
+static CRB_Value
+call_native_function(CRB_Interpreter *inter, LocalEnvironment *env, Expression *expr, CRB_NativeFunctionProc *proc) {
+    CRB_Value value;
+    int arg_count;
+    ArgumentList *arg_p;
+    CRB_Value *args;
+    int i;
+
+    for (arg_count = 0, arg_p = expr->u.function_call_expression.argument; arg_p; arg_p = arg_p->next) {
+        arg_count++;
+    }
+    args = MEM_malloc(sizeof(CRB_Value) * arg_count);
+
+    for (arg_p = expr->u.function_call_expression.argument, i = 0; arg_p; arg_p = arg_p->next, i++) {
+        args[i] = eval_expression(inter, env, arg_p->expression);
+    }
+    value = proc(inter, arg_count, args);
+    for (i = 0; i < arg_count; i++) {
+        release_if_string(&args[i]);
+    }
+    MEM_free(args);
+    return value;
+}
+
+static CRB_Value
+call_crowbar_function(CRB_Interpreter *inter, LocalEnvironment *env, Expression *expr, FunctionDefinition *func) {
+    CRB_Value value;
+    StatementResult result;
+    ArgumentList *arg_p;
+    ParameterList *param_p;
+    LocalEnvironment *local_env;
+
+    local_env = alloc_local_envirnoment();
+
+    for (arg_p = expr->u.function_call_expression.argument, param_p = func->u.crowbar_f.parameter; arg_p; arg_p = arg_p->next, param_p = param_p->next) {
+        CRB_Value arg_val;
+        if (param_p == NULL) {
+            crb_runtime_error(expr->line_number, ARGUMENT_TOO_MANY_ERR, MESSAGE_ARGUMENT_END);
+        }
+        arg_val = eval_expression(inter, env, arg_p->expression);
+        crb_add_local_variable(local_env, param_p->name, &arg_val);
+    }
+
+
+    if (param_p) {
+        crb_runtime_error(expr->line_number, ARGUMENT_TOO_FEW_ERR, MESSAGE_ARGUMENT_END);
+    }
+    result = crb_execute_statement_list(inter, local_env, func->u.crowbar_f.block->statement_list);
+
+    if (result.type == RETURN_STATEMENT_RESULT) {
+        value = result.u.return_value;
+    } else {
+        value.type = CRB_NULL_VALUE;
+    }
+
+    dispose_local_environment(inter, local_env);
+    return value;
+}
+
+static CRB_Value eval_function_call_expression(CRB_Interpreter *inter, LocalEnvironment *env, Expression *expr) {
+    CRB_Value value;
+    FunctionDefinition *func;
+
+    char *identifier = expr->u.function_call_expression.identifier;
+
+    func = crb_search_function(identifier);
+    if (func == NULL) {
+        crb_runtime_error(expr->line_number, FUNCTION_NOT_FOUND_ERR, STRING_MESSAGE_ARGUMENT, "name", identifier,
+                          MESSAGE_ARGUMENT_END);
+    }
+
+    switch (func->type) {
+        case CROWBAR_FUNCTION_DEFINITION:
+            value = call_crowbar_function(inter, env, expr, func);
+            break;
+        case NATIVE_FUNCTION_DEFINITION:
+            value = call_native_function(inter, env, expr, func->u.native_f.proc);
+            break;
+        default:
+            DBG_panic(("bad case...%d\n", func->type));
+    }
+    return value;
+}
+
+static CRB_Value eval_expression(CRB_Interpreter *inter, LocalEnvironment *env, Expression *expr) {
+    CRB_Value v;
+    switch (expr->type) {
+        case BOOLEAN_EXPRESSION:
+            v = eval_boolean_expression(expr->u.boolean_value);
+            break;
+        case INT_EXPRESSION:
+            v = eval_int_expression(expr->u.int_value);
+            break;
+        case DOUBLE_EXPRESSION:
+            v = eval_double_expression(expr->u.double_value);
+        case STRING_EXPRESSION:
+            v = eval_string_expression(inter, expr->u.string_value);
+            break;
+        case IDENTIFIER_EXPRESSION:
+            v = eval_identifier_expression(inter, env, expr);
+            break;
+        case ASSIGN_EXPRESSION:
+            v = eval_assign_expression(inter, env, expr->u.assign_expression.variable,
+                                       expr->u.assign_expression.operand);
+            break;
+        case ADD_EXPRESSION:
+        case SUB_EXPRESSION:
+        case MUL_EXPRESSION:
+        case DIV_EXPRESSION:
+        case MOD_EXPRESSION:
+        case EQ_EXPRESSION:
+        case NE_EXPRESSION:
+        case GT_EXPRESSION:
+        case GE_EXPRESSION:
+        case LT_EXPRESSION:
+        case LE_EXPRESSION:
+            v = crb_eval_binary_expression(inter, env, expr->type, expr->u.binary_expression.left,
+                                           expr->u.binary_expression.right);
+        case LOGICAL_AND_EXPRESSION:
+        case LOGICAL_OR_EXPRESSION:
+            v = eval_logical_and_or_expression(inter, env, expr->type, expr->u.binary_expression.left,
+                                               expr->u.binary_expression.right);
+            break;
+        case MINUS_EXPRESSION:
+            v = crb_eval_minus_expression(inter, env, expr->u.minus_expression);
+            break;
+        case FUNCTION_CALL_EXPRESSION:
+            v = eval_function_call_expression(inter, env, expr);
+            break;
+        case NULL_EXPRESSION:
+            v = eval_null_expression();
+        case EXPRESSION_TYPE_COUNT_PLUS_1:
+        default:
+            DBG_panic(("bad case. type..%d\n", expr->type));
+    }
+    return v;
+}
+
+CRB_Value crb_eval_expression(CRB_Interpreter *inter, LocalEnvironment *env, Expression *expr) {
+    return eval_expression(inter, env, expr);
+}
